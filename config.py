3708422e45b3c022cea705add5a227c6442c9a18
hm <hmatthyseniv@gmail.com>
2019-08-16 08:56:30 -0500

    Add: Conifig file for globals

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 15:19:20 2019
+
+config file
+
+@author: hmatthyseniv
+"""
+
+SCREEN = {
+    "TITLE" : "The Menu Game - WIP",
+    "WIDTH" : 800,
+    "HEIGHT" : 1000
+
+    }
+
+PS_DEFAULTS = {
+        "SIZE" : 8,
+        "COLOR_MAP" : ["White","Black"]
+
+        }
